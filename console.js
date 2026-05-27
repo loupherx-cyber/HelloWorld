6f2f5683775bc71a24aa35a24b4fed510d97d2ef
loupherx-cyber <loupherx@gmail.com>
2026-05-27 18:41:01 -0500

    Update: Change output to 10 is greater than 5

--- console.js
+++ console.js
@@ -1,8 +1,8 @@
 console.log('Hello World!');
 
 // if true 
-if  ( 4 > 3) {
-    console.log('4 is greater than 3');
+if  ( 10 > 5) {
+    console.log('10 is greater than 5');
 }   else {
-    console.log('4 is not greater than 3');
+    console.log('10 is not greater than 5');
 }
